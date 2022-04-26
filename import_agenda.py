afaf58b49e5065b1e9fe6a77ee7acbb361024f6e
Frank Lai <frank810618@gmail.com>
2022-04-25 23:09:21 -0700

    adjust table columns and handle speaker table

--- import_agenda.py
+++ import_agenda.py
@@ -1,5 +1,3 @@
-from xlrd.timemachine import xrange
-
 from db_table import db_table
 import xlrd
 
@@ -7,30 +5,59 @@ file_name = "agenda.xls"
 agenda = xlrd.open_workbook(file_name).sheet_by_name("Agenda")
 schema = [x.replace("*", "").replace("\n", "") for x in agenda.row_values(14)]
 
-users = db_table("agenda", {
+agenda_Table = db_table("agenda", {
     "id": "integer PRIMARY KEY",
     "date": "string NOT NULL",
     "time_start": "string NOT NULL",
     "time_end": "string NOT NULL",
-    "session_title": "string NOT NULL",
+    "title": "string NOT NULL",
     "location": "string",
-    "description": "string"
+    "description": "string",
+    "speaker": "string",
+    "parent_id": "integer"
+})
+
+speaker_table = db_table("speaker", {
+    "agenda_id": "integer NOT NULL",
+    "name": "string NOT NULL"
 })
 
 id = 1
-for rx in xrange(15, agenda.nrows):
+parent_id = 1
+for rx in range(15, agenda.nrows):
     content = [c for c in agenda.row_values(rx)]
 
-    users.insert({
+    session = content[3]
+    if session == "Session":
+        parent_id = id
+
+    # insert into agenda table
+    agenda_Table.insert({
         "id": id,
         "date": content[0],
         "time_start": content[1],
         "time_end": content[2],
-        "session_title": content[3],
-        "location": content[4],
-        "description": content[5]
+        "title": content[4],
+        "location": content[5],
+        "description": content[6],
+        "parent_id": parent_id if session == "Sub" else None
     })
+
+    # insert into speaker table, using agenda_id to refer to data in agenda table
+    if ";" in content[7]:
+        speakers = content[7].split(';')
+        for speaker in speakers:
+            speaker_table.insert({
+                "agenda_id": id,
+                "name": speaker.strip()
+            })
+    elif len(content[7]) > 0:
+        speaker_table.insert({
+            "agenda_id": id,
+            "name": content[7]
+        })
 
     id += 1
 
-users.close()
+agenda_Table.close()
+speaker_table.close()
